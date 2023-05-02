980a86a5dc0edc137051db0b8e3bd9cd9045f22b
RobertoZefi <zefi.roberto2001@gmail.com>
2023-05-02 17:17:47 +0200

    FEAT milestone 4 done

---
+++ src/components/Project.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default{
+    data(){
+        return{
+            projects: [],
+        }
+    },
+
+    methods: {
+        fetchProjects(){
+            axios.get('http://127.0.0.1:8000/api/projects')
+            .then(res => {
+                console.log(res)
+                
+            })
+        }
+    },
+
+    mounted(){
+        this.fetchProjects()
+    }
+
+    
+}
+</script>
+
+<style>
+
+</style>
